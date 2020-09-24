d3475d3386b61dc1314fff92ded9c164f0d50155
Jose <josealeleyva16@gmail.com>
2020-09-24 19:39:33 -0300

    Habilitacion de los CORS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors')
 
 //settings
 app.set('port', process.env.PORT || 3500);
@@ -9,8 +10,16 @@ app.set('port', process.env.PORT || 3500);
 //middleware
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors())
 
 //global variables
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 app.use((req, res, next)=>{
     next();
 });
